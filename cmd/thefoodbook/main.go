5a19f904b916da00c23329d82f658a5db9c3eb84
maria <maria.ic.mihai@gmail.com>
2023-01-27 09:28:02 +0100

    Reorganize configuration and main

--- cmd/thefoodbook/main.go
+++ cmd/thefoodbook/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"context"
 	"github.com/mariamihai/thefoodbook/configs"
+	"log"
+	"time"
 )
 
 func main() {
-	_ = configs.InitializeConfiguration()
+	c, err := configs.NewConfig()
+	if err != nil {
+		log.Fatalf("Error encountered when loading the configuration: %v", err)
+	}
+
+	ctxWithTimeout, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	driver, err := configs.NewDriver(ctxWithTimeout, c.DBUri, c.DBUser, c.DBPass)
+
+	if err != nil {
+		log.Fatalf("Couldn't connect to the db: %v", err)
+	}
+
+	err = configs.CloseDriver(ctxWithTimeout, driver)
+	if err != nil {
+		log.Fatalf("Couldn't close the db: %v", err)
+	}
 }
